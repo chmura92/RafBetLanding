940e6c7df8f8c265f4115eb48c52973907216680
Roman Dużynski <roman.duzynski@portacapena.com>
2026-05-12 01:12:21 +0200

    feat: sitemap przez @astrojs/sitemap + robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://posadzki-wylewki.opole.pl',
+  integrations: [sitemap()],
   output: 'static',
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' },
